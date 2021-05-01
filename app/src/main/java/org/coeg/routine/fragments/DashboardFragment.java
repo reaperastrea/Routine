ed2f0635514e743cd8183d295581b063e76b5df7
CainSiaanr <79513486+CainSiaanr@users.noreply.github.com>
2021-05-02 00:43:16 +0700

    Showing user data from preferences

--- app/src/main/java/org/coeg/routine/fragments/DashboardFragment.java
+++ app/src/main/java/org/coeg/routine/fragments/DashboardFragment.java
@@ -1,5 +1,6 @@
 package org.coeg.routine.fragments;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,9 +15,12 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.imageview.ShapeableImageView;
+
 import org.coeg.routine.adapters.RoutineListAdapter;
 import org.coeg.routine.animations.AccuracyAnimation;
 import org.coeg.routine.R;
+import org.coeg.routine.backend.PreferencesStorage;
 import org.coeg.routine.backend.Routine;
 
 import java.util.LinkedList;
@@ -25,10 +29,19 @@ public class DashboardFragment extends Fragment
 {
     private static final int ANIMATION_DURATION = 1500;
 
-    private TextView        txtName;
-    private TextView        txtAccuracy;
-    private ProgressBar     pbAccuracy;
-    private RecyclerView    rvRecentRoutine;
+    private TextView            txtName;
+    private TextView            txtAccuracy;
+    private ShapeableImageView  imgUser;
+    private ProgressBar         pbAccuracy;
+    private RecyclerView        rvRecentRoutine;
+
+    private String  name;
+    private String  profilePicturePath;
+    private Integer lateCount;
+    private Integer onTimeCount;
+    private Integer routineCount;
+    private Integer accuracy;
+
     RoutineListAdapter mAdapter;
     LinkedList<Routine> routineList = new LinkedList<>();
 
@@ -37,6 +50,7 @@ public class DashboardFragment extends Fragment
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
     {
+        FetchPreferences();
         InitView(view);
         routineListTest();
         mAdapter = new RoutineListAdapter(this.getContext(), routineList);
@@ -62,11 +76,15 @@ public class DashboardFragment extends Fragment
         txtAccuracy     = view.findViewById(R.id.txt_userAccuracy);
         pbAccuracy      = view.findViewById(R.id.progress_accuracy);
         rvRecentRoutine = view.findViewById(R.id.rv_recentRoutine);
+        imgUser         = view.findViewById(R.id.imgUser);
 
         // Set view data from database
+        txtName.setText(name);
+        txtAccuracy.setText(accuracy.toString());
+        imgUser.setImageBitmap(BitmapFactory.decodeFile(profilePicturePath));
 
         //Play animation according
-        int accuracy = 100;         // FOR DEBUG PURPOSE
+        //int accuracy = 100;         // FOR DEBUG PURPOSE //I've made the global variable version of this - ivan
         PlayAnimation(accuracy);
     }
 
@@ -83,5 +101,16 @@ public class DashboardFragment extends Fragment
 
     public void routineListTest(){
         //routineList.add(new Routine());
+    }
+
+    private void FetchPreferences(){
+        PreferencesStorage preferences = PreferencesStorage.getInstance();
+        preferences.loadPreferences(this.getContext());
+        name = preferences.getFullName();
+        profilePicturePath = preferences.getProfilePicturePath();
+        lateCount = preferences.getLateCounter();
+        onTimeCount = preferences.getOnTimeCounter();
+        routineCount = lateCount+onTimeCount;
+        accuracy = (onTimeCount/routineCount)*100;
     }
 }
