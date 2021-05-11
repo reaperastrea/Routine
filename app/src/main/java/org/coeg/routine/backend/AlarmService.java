5eac67f34cd95cf602bf3ce7d38702f08c777b68
Lemuel Lancaster <32617101+Itsakaseru@users.noreply.github.com>
2021-05-11 13:15:40 +0700

    Tidying up code for alarm service

--- app/src/main/java/org/coeg/routine/backend/AlarmService.java
+++ app/src/main/java/org/coeg/routine/backend/AlarmService.java
@@ -25,17 +25,15 @@ import org.coeg.routine.activities.MainActivity;
 public class AlarmService extends Service
 {
     public static final String CHANNEL_ID = "ALARM_SERVICE_CHANNEL";
-    public static final String CHANNEL_NAME = "Routine Notification Channel";
+    public static final String CHANNEL_NAME = "Routine";
     public static final String CHANNEL_DESC = "Routine Remainder";
     public static final int NOTIFICATION_ID = 1337;
 
     private static final int ACTION_OK = 0;
     private static final int ACTION_SNOOZE = 1;
 
-    private String routineName;
-    private int routineID;
-
-    NotificationManagerCompat notificationManager;
+    private NotificationManagerCompat notificationManager;
+    private Notification notification;
 
     private MediaPlayer mediaPlayer;
 
@@ -51,53 +49,9 @@ public class AlarmService extends Service
     @Override
     public int onStartCommand(Intent intent, int flags, int startId)
     {
-        routineName = intent.getStringExtra("Routine Name");
-        routineID = intent.getIntExtra("Routine ID", -1);
-
-        long[] vibratePattern = new long[] { 500, 500, 500 };
-
-        Intent receiver = new Intent(this, NotificationActionReceiver.class);
-        PendingIntent pOkIntent = PendingIntent.getBroadcast(
-                this, ACTION_OK, receiver.putExtra("Request Code", ACTION_OK).putExtra("Routine ID", routineID), 0);
-
-        PendingIntent pSnoozeIntent = PendingIntent.getBroadcast(
-                this, ACTION_SNOOZE, receiver.putExtra("Request Code", ACTION_SNOOZE).putExtra("Routine ID", routineID), 0);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(), 0);
-
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
-                // To enable heads up notification
-                .setPriority(NotificationCompat.PRIORITY_MAX)
-                .setVibrate(vibratePattern)
-                // Heads up notification will be persistent until action given
-                .setFullScreenIntent(pendingIntent, true)
-                .setContentTitle("Routine Remainder")
-                .setContentText(routineName)
-                .setSmallIcon(R.drawable.ic_logo_routine)
-                .setOngoing(true)
-                .addAction(0, getString(R.string.notification_ok), pOkIntent)
-                .addAction(0, getString(R.string.notification_snooze), pSnoozeIntent)
-                .build();
-
-        notificationManager = NotificationManagerCompat.from(this);
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
-        {
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance);
-            mChannel.setDescription(CHANNEL_DESC);
-            mChannel.enableVibration(true);
-            mChannel.setVibrationPattern(vibratePattern);
-
-            notificationManager.createNotificationChannel(mChannel);
-        }
-
+        setupNotification(intent);
         startForeground(1, notification);
-
         mediaPlayer.start();
-
-        Log.i("DEBUG-TEST", "ALARM SERVICE RUNNING");
-        Log.i("Routine ID", "Routine ID : " + routineID);
 
         return START_NOT_STICKY;
     }
@@ -116,5 +70,58 @@ public class AlarmService extends Service
     public IBinder onBind(Intent intent)
     {
         return null;
+    }
+
+    private void setupNotification(Intent intent)
+    {
+        String routineName = intent.getStringExtra("Routine Name");
+        int routineID = intent.getIntExtra("Routine ID", -1);
+
+        long[] vibratePattern = new long[] { 500, 500, 500 };
+
+        Intent receiver = new Intent(this, NotificationActionReceiver.class);
+        PendingIntent pOkIntent = PendingIntent.getBroadcast(
+                this, ACTION_OK, receiver
+                        .putExtra("Request Code", ACTION_OK)
+                        .putExtra("Routine ID", routineID), 0);
+
+        PendingIntent pSnoozeIntent = PendingIntent.getBroadcast(
+                this, ACTION_SNOOZE, receiver
+                        .putExtra("Request Code", ACTION_SNOOZE)
+                        .putExtra("Routine ID", routineID), 0);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(), 0);
+
+        notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                // To enable heads up notification
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setVibrate(vibratePattern)
+                .setSound(null)
+                // Make Heads up notification will be persistent until action given
+                .setFullScreenIntent(pendingIntent, true)
+                .setContentTitle("Routine Remainder")
+                .setContentText(routineName)
+                .setSmallIcon(R.drawable.ic_logo_routine)
+                .setOngoing(true)
+                .addAction(0, getString(R.string.notification_ok), pOkIntent)
+                .addAction(0, getString(R.string.notification_snooze), pSnoozeIntent)
+                .build();
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+        // For API >26
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
+        {
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            mChannel.setSound(null, null);
+            mChannel.setDescription(CHANNEL_DESC);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(vibratePattern);
+
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        Log.i("DEBUG-TEST", "ALARM SERVICE RUNNING");
+        Log.i("Routine ID", "Routine ID : " + routineID);
     }
 }
