1e6a2108f5e7492703b09a73bee3065b165a45b4
Lemuel Lancaster <32617101+Itsakaseru@users.noreply.github.com>
2021-05-14 13:03:12 +0700

    Move schedule method to Routine class for ease of use.

--- app/src/main/java/org/coeg/routine/backend/Routine.java
+++ app/src/main/java/org/coeg/routine/backend/Routine.java
@@ -1,13 +1,21 @@
 package org.coeg.routine.backend;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class Routine {
@@ -69,5 +77,111 @@ public class Routine {
         builder.append(active);
 
         return builder.toString();
+    }
+
+    /**
+     * Schedule routine to alarm manager
+     * @param context application context
+     * @apiNote PLEASE CHECK TO MAKE SURE THERE AREN'T ANY OF THE SAME ROUTINE WITH
+     * THE SAME DAY + HOURS IN THE DATABASE BEFORE CALLING THIS METHOD
+     */
+    public void schedule(Context context)
+    {
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        Days[] days = getDays();
+        int routineID = id;
+        String routineName = name;
+
+        Calendar routineTime = Calendar.getInstance();
+
+        try
+        {
+            routineTime.setTime(Objects.requireNonNull(formatter.parse(time)));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        // Schedule for each selected days
+        for (Days day : days)
+        {
+            Calendar scheduleTime = Calendar.getInstance();
+
+            // Set current calendar to current system time
+            scheduleTime.setTimeInMillis(System.currentTimeMillis());
+            scheduleTime.set(Calendar.HOUR_OF_DAY, routineTime.get(Calendar.HOUR_OF_DAY));
+            scheduleTime.set(Calendar.MINUTE, routineTime.get(Calendar.MINUTE));
+            scheduleTime.set(Calendar.SECOND, 0);
+            scheduleTime.set(Calendar.MILLISECOND, 0);
+            scheduleTime.set(Calendar.DAY_OF_MONTH, Calendar.DAY_OF_MONTH);
+
+            switch(day)
+            {
+                case Sunday:
+                    scheduleTime.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+                    break;
+
+                case Monday:
+                    scheduleTime.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+                    break;
+
+                case Tuesday:
+                    scheduleTime.set(Calendar.DAY_OF_WEEK, Calendar.TUESDAY);
+                    break;
+
+                case Wednesday:
+                    scheduleTime.set(Calendar.DAY_OF_WEEK, Calendar.WEDNESDAY);
+                    break;
+
+                case Thursday:
+                    scheduleTime.set(Calendar.DAY_OF_WEEK, Calendar.THURSDAY);
+                    break;
+
+                case Friday:
+                    scheduleTime.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
+                    break;
+
+                case Saturday:
+                    scheduleTime.set(Calendar.DAY_OF_WEEK, Calendar.SATURDAY);
+                    break;
+            }
+
+            Log.i("Routine", "----- Scheduling -----");
+
+            // Check if whether the day selected has passed, if yes then
+            // add 7 days into the scheduler
+            if (scheduleTime.getTimeInMillis() <= System.currentTimeMillis())
+            {
+                scheduleTime.add(Calendar.DAY_OF_MONTH, 7);
+                Log.i("Routine", "ADD 7 DAYS");
+            }
+
+            // DEBUG LOG
+            Log.i("Routine", "Day     : " + scheduleTime.get(Calendar.DAY_OF_WEEK));
+            Log.i("Routine", "Hours   : " + scheduleTime.get(Calendar.HOUR_OF_DAY));
+            Log.i("Routine", "Minutes : " + scheduleTime.get(Calendar.MINUTE));
+            Log.i("Routine", "DOM     : " + scheduleTime.get(Calendar.DAY_OF_MONTH));
+            Log.i("Routine", "Month   : " + scheduleTime.get(Calendar.MONTH));
+
+            // Intent to handle broadcast from alarm manager
+            Intent intent = new Intent(context, AlarmBroadcastReceiver.class);
+            intent.putExtra("Routine ID", routineID);
+            intent.putExtra("Routine Name", routineName);
+
+            PendingIntent alarmPendingIntent = PendingIntent.getBroadcast(context, routineID, intent, 0);
+
+            // Routine will always repeating
+            int RUN_WEEKLY = 24 * 7 * 60 * 60 * 1000;
+
+            // Schedule to alarm manager
+            alarmManager.setRepeating(
+                    AlarmManager.RTC_WAKEUP,
+                    scheduleTime.getTimeInMillis(),
+                    RUN_WEEKLY,
+                    alarmPendingIntent
+            );
+        }
     }
 }
