fcbf4b1f4139f916324d3dc26f054da0bcb3f70a
reaperastrea <55103903+reaperastrea@users.noreply.github.com>
2021-05-16 15:48:45 +0700

    trying addroutine

--- app/src/main/java/org/coeg/routine/activities/AddRoutineActivity.java
+++ app/src/main/java/org/coeg/routine/activities/AddRoutineActivity.java
@@ -3,6 +3,7 @@ package org.coeg.routine.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Rect;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -11,7 +12,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
@@ -21,6 +24,20 @@ import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.shawnlin.numberpicker.NumberPicker;
 
 import org.coeg.routine.R;
+import org.coeg.routine.adapters.RoutineListAdapter;
+import org.coeg.routine.backend.Routine;
+import org.coeg.routine.backend.RoutineDatabase;
+import org.coeg.routine.backend.RoutinesHandler;
+import org.coeg.routine.fragments.ListFragment;
+
+import java.lang.reflect.Array;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 public class AddRoutineActivity extends AppCompatActivity
 {
@@ -34,11 +51,15 @@ public class AddRoutineActivity extends AppCompatActivity
     private EditText            etRoutineName;
     private SwitchMaterial      swSnooze;
 
+    private RoutineListAdapter  mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_routine);
+
+        //new dbasnc().execute(this.getContext());
 
         InitView();
         InitListener();
@@ -87,6 +108,13 @@ public class AddRoutineActivity extends AppCompatActivity
         npMinute.setTypeface(ResourcesCompat.getFont(this, R.font.nunito_extrabold));
         npMinute.setSelectedTypeface(ResourcesCompat.getFont(this, R.font.nunito_extrabold));
 
+        //Set Value NumberPicker
+        npHour.setMaxValue(23);
+        npHour.setMinValue(00);
+
+        npMinute.setMaxValue(59);
+        npMinute.setMinValue(0);
+
         //Set The Header Text View's Content
         Intent intent = getIntent();
         if(intent.getStringExtra("Update Routine").equals("Update Routine")){
@@ -99,9 +127,20 @@ public class AddRoutineActivity extends AppCompatActivity
      */
     private void InitListener()
     {
+        //Day Picker
+        dpDay.selectAllDays();
+        dpDay.setDayPressedListener(new MaterialDayPicker.DayPressedListener() {
+            @Override
+            public void onDayPressed(@NonNull MaterialDayPicker.Weekday weekday, boolean isSelected) {
+                dpDay.setDayEnabled(weekday, isSelected);
+            }
+        });
+
         // Check and add user data to database
         btnAddRoutine.setOnClickListener(v -> {
-
+            npHour.getValue();
+            npMinute.getValue();
+            dpDay.getSelectedDays();
         });
 
         // Exit the Add Routine activity
@@ -110,4 +149,44 @@ public class AddRoutineActivity extends AppCompatActivity
             finish();
         });
     }
+
+    private class dbadd extends AsyncTask<Context, Integer, List<String>> {
+        List<String>        selectedDays;
+        boolean             exists;
+        RoutinesHandler     handler;
+
+        @Override
+        protected List<String> doInBackground(Context... contexts) {
+            /*handler = new RoutinesHandler(context[0]);
+            handler.addRoutine(dpDay, npHour, npMinute);*/
+            try {
+
+                exists = false;
+                /*for(int i = 0; i < selectedDays.contains(dpDay + npHour + npMinute); i++)
+                {
+                    checkIfExists(selectedDays.get(i), npHour)
+                }*/
+
+                if (exists)
+                    Toast.makeText(getApplicationContext(), "Theres already a routine overlap that day and time", Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return (List<String>) handler;
+        }
+
+        private void checkIfExists(String s, NumberPicker npHour, MaterialDayPicker dpDay) {
+            if (selectedDays.contains(npHour) && selectedDays.contains(dpDay))
+                exists = true;
+        }
+    }
+
+    /*private void removeDuplicates() {
+        List<MaterialDayPicker> arr = Arrays.asList(dpDay);
+        List<NumberPicker> brr = Arrays.asList(npHour, npMinute);
+        Set<Integer> hashSet = new LinkedHashSet(arr);
+        Set<Float> hashSet1 = new LinkedHashSet(brr);
+        ArrayList<Integer> removedDuplicates1 = new ArrayList(hashSet);
+        ArrayList<Integer> removedDuplicates2 = new ArrayList(hashSet1);
+    }*/
 }
