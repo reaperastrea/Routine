a5531a5804069a90be0637bf5c7e6c92b84dfacf
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2021-05-17 21:47:15 +0700

    Remove notifications settings

--- app/src/main/java/org/coeg/routine/fragments/SettingFragment.java
+++ app/src/main/java/org/coeg/routine/fragments/SettingFragment.java
@@ -25,14 +25,9 @@ public class SettingFragment extends Fragment
     private TextView btnDeleteEverything;
 
     private SwitchMaterial swDiagnostic;
-    private SwitchMaterial swPushNotification;
-    private SwitchMaterial swPrePushNotification;
 
     PreferencesStorage preferences;
-
     private boolean enableTelemetry;
-    private boolean enablePushNotifications;
-    private boolean enablePreReminder;
 
     public SettingFragment() { }
 
@@ -63,8 +58,6 @@ public class SettingFragment extends Fragment
         btnDeleteEverything = view.findViewById(R.id.btn_deleteEverything);
 
         swDiagnostic = view.findViewById(R.id.switch_diagnostic);
-        swPushNotification = view.findViewById(R.id.switch_pushNotification);
-        swPrePushNotification = view.findViewById(R.id.switch_prePushNotification);
 
         preferences = PreferencesStorage.getInstance();
         loadPreferences();
@@ -72,24 +65,7 @@ public class SettingFragment extends Fragment
         swDiagnostic.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                Log.i(getClass().toString(), "enableTelemetry = " + b);
                 enableTelemetry = b;
-                savePreferences();
-            }
-        });
-        swPushNotification.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                Log.i(getClass().toString(), "enablePushNotifications = " + b);
-                enablePushNotifications = b;
-                savePreferences();
-            }
-        });
-        swPrePushNotification.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                Log.i(getClass().toString(), "enablePreReminder = " + b);
-                enablePreReminder = b;
                 savePreferences();
             }
         });
@@ -119,23 +95,12 @@ public class SettingFragment extends Fragment
 
     private void loadPreferences() {
         preferences.loadPreferences(getContext());
-
-        // load
         enableTelemetry = preferences.isTelemetryEnabled();
-        enablePushNotifications = preferences.isPushNotificationsEnabled();
-        enablePreReminder = preferences.isPreReminderEnabled();
-        Log.i(getClass().toString(), "loaded = " + enableTelemetry + " " + enablePushNotifications + " " + enablePreReminder);
-
-        // set switches
         swDiagnostic.setChecked(enableTelemetry);
-        swPushNotification.setChecked(enablePushNotifications);
-        swPrePushNotification.setChecked(enablePreReminder);
     }
 
     private void savePreferences() {
         preferences.setEnableTelemetry(enableTelemetry);
-        preferences.setEnablePushNotifications(enablePushNotifications);
-        preferences.setEnablePreReminder(enablePreReminder);
         preferences.savePreferences();
     }
 }
